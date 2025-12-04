f8efe88866491822867c26cc539340236f10dcdd
moviefan322 <philman202@yahoo.com>
2025-12-04 00:42:54 -0500

    adds server running text

--- src/app.js
+++ src/app.js
@@ -58,6 +58,10 @@ if (config.env === 'production') {
 // v1 api routes
 app.use('/v1', routes);
 
+app.get('/', (req, res) => {
+  res.send('server is running');
+});
+
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
